d315f3c9ca0501f44a62bd1293b943f6377425aa
jdpena28 <johnhenrich1128@gmail.com>
2022-02-13 13:51:35 +0800

    add font family inter & source sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      primary: ["Inter", "sans-serif"],
+      secondary: ["Source Sans Pro", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
