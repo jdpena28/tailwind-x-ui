62569fa7b3219d33ed0ef582d69b7c82a08cfbbf
jdpena28 <johnhenrich1128@gmail.com>
2022-02-13 14:05:05 +0800

    Add background image

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,27 +3,13 @@ import { NextSeo } from "next-seo";
 
 const Home: NextPage = () => {
   return (
-    <section className="flex h-screen w-full items-center justify-center">
+    <section
+      style={{ backgroundImage: "url(./Background.png)" }}
+      className="flex h-screen w-full items-center justify-center bg-cover bg-no-repeat">
       <NextSeo
-        title="Next.js Boilerplate"
-        description="Next.js Boilerplate configuration with Eslint, Prettier and Husky"
+        title="Tailwind-X-UI"
+        description="An on the go jsx component collections for Tailwind CSS"
       />
-      <div className="text-center">
-        <h1 className="text-3xl font-semibold">Next.js Boilerplate</h1> <br />
-        <p>Configuration with Eslint, Prettier and Husky</p>
-        <ul className="list-disc !text-left">
-          <li className="">
-            <a href="https://github.com/tailwindlabs/prettier-plugin-tailwindcss">
-              Tailwind Uniformed Class Order
-            </a>
-          </li>
-          <li>
-            <a href="https://typescript-eslint.io/docs/linting/">
-              TS Lint Parser
-            </a>
-          </li>
-        </ul>
-      </div>
     </section>
   );
 };
